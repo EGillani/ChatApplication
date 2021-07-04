e7ec67e99c94ddee4cd372fb2cde3934385ffa49
Eraj Gillani <eraj.gillani@hotmail.com>
2021-07-04 16:54:19 -0400

    fixed dependancies issue

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import axios from 'axios';
+import mlhLogo from '../images/mlh-logo-color.png';
+
+const projectID = 'f9386a29-08f1-40fd-87aa-7c0ffade6b39';
+
+const Modal = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, incorrect credentials.');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <img className="logo" src={mlhLogo} />
+        <h2 className="title">Chat Application</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>LogIn</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+
+  );
+};
+
+export default Modal;
